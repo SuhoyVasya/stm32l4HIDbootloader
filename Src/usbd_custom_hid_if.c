b96b7d8b70846253455ff08b958783f6d6888a26
alex <walklikeninja@yandex.ru>
2019-09-09 16:26:09 +0400

    formate code

--- Src/usbd_custom_hid_if.c
+++ Src/usbd_custom_hid_if.c
@@ -8,9 +8,10 @@ extern FlagStatus USBDatainReady;
 /** Usb HID report descriptor. */
 __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DESC_SIZE] __ALIGN_END =
 {
-	    0x06, 0x00, 0xff,              // USAGE_PAGE (Generic Desktop)
-	    0x09, 0x01,                    // USAGE (Vendor Usage 1)
-	    0xa1, 0x01,                    // COLLECTION (Application)
+	    0x06, 0x00, 0xff,              // 	USAGE_PAGE (Generic Desktop)
+	    0x09, 0x01,                    // 	USAGE (Vendor Usage 1)
+	    0xa1, 0x01,                    // 	COLLECTION (Application)
+	
 	    0x85, 0x01,                    //   REPORT_ID (1)
 	    0x09, 0x01,                    //   USAGE (Vendor Usage 1)
 	    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
@@ -38,7 +39,7 @@ __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DES
 	    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
 	    0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
 	    0x75, 0x08,                    //   REPORT_SIZE (8)
-	    0x95, ARRAY2ST, //RPT3_COUNT,                    //   REPORT_COUNT (N)
+	    0x95, ARRAY2ST, 							 //		RPT3_COUNT, //   REPORT_COUNT (N)
 	    0xb1, 0x82,                    //   FEATURE (Data,Var,Abs,Vol)
 	    0x85, 0x03,                    //   REPORT_ID (3)
 	    0x09, 0x03,                    //   USAGE (Vendor Usage 3)
@@ -47,9 +48,9 @@ __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DES
 	    0x85, 0x04,                    //   REPORT_ID (4)
 	    0x09, 0x04,                    //   USAGE (Vendor Usage 4)
 	    0x75, 0x08,                    //   REPORT_SIZE (8)
-	    0x95, ARRAY2HOST, //RPT4_COUNT,                    //   REPORT_COUNT (N)
+	    0x95, ARRAY2HOST, 						 //		RPT4_COUNT,  //   REPORT_COUNT (N)
 	    0x81, 0x82,                    //   INPUT (Data,Var,Abs,Vol)
-  0xC0    /*     END_COLLECTION	             */
+			0xC0    											 //		END COLLECTION
 };
 
 USBD_HandleTypeDef  *hUsbDevice_0;	// Need for CUSTOM_HID_OutEvent_FS
